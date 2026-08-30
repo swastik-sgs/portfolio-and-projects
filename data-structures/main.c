08a8a0f56f4ab0ad9e60de73bcb168f5427578dd
swastik-sgs <itsswastikstudy@gmail.com>
2026-08-30 15:03:39 +0530

    add

--- data-structures/main.c
+++ data-structures/main.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
-  int n;
-  printf("enter number n: ");
-  scanf("%d", & n);
-
-  printf("%d", n%2==0);
+  int israining= 1;
+  int ismonday= 0;
+  printf(israining ||  ismonday);
   return 0;
 }
