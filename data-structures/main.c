009f613abab1b904a822c36ed4082784fb69b678
swastik-sgs <itsswastikstudy@gmail.com>
2026-08-30 12:32:39 +0530

    add

--- data-structures/main.c
+++ data-structures/main.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-int main() {
-  int a;
-  printf("write any number a: ");
-  scanf("%d", & a);
+int main(){
+  int n;
+  printf("write a number n: ");
+  scanf("%d", & n);
 
-  int b;
-  printf("write any number b: ");
-  scanf("%d", & b);
-
-  printf("perimeter of rectangle is %d", 2*(a+b));
-  return 0;
+  printf("cube of number n is %d" , n*n*n);
+  return 0 ;
 }
