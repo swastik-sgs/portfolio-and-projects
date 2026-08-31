56fe6d286db99796676f4a570bc6cccd450c5744
swastik-sgs <itsswastikstudy@gmail.com>
2026-08-31 17:07:09 +0530

    Update main.c

--- data-structures/main.c
+++ data-structures/main.c
@@ -1,19 +1,9 @@
 #include<stdio.h>
-#include<math.h>
-int main() {
-  int a;
-  printf("write a number1: ");
-  scanf("%d", &a);
+int main(){
+  int age;
+  printf("enter age : ");
+  scanf("%d", &age);
 
-  int b;
-  printf("write a number2: ");
-  scanf("%d", &b);
-
-   int c;
-  printf("write a number3: ");
-  scanf("%d", &c);
-
-  printf("avg is %d", (a+b+c)/3);
+  age>=18 ? printf("adult: \n") : printf("not adult: \n");
   return 0;
-
 }
